b03f4d074a81bb6ca0aaab6fa5020eac24f44cc5
Gustavo <gustavo645.gg.gg@gmail.com>
2024-06-08 17:48:42 -0500

    test pie

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { createServer } = require('node:http');
+const { readFileSync } = require('node:fs');
+const { resolve } = require('node:path');
 const hostname = '0.0.0.0';
 const port = 8094;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Soy el grupo 4');
+  const htmlContent = readFileSync(resolve(__dirname, 'index.html'), 'utf8');
+  res.end(htmlContent);
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
