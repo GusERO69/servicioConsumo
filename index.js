25ac6a7167cbc21ef5312518a187c14c73e822a4
Gustavo <gustavo645.gg.gg@gmail.com>
2024-06-13 22:01:44 -0500

    add gitignore

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { createServer } = require('node:http');
-const hostname = '0.0.0.0';
-const port = 8094;
+const { createServer } = require("node:http");
+const hostname = "127.0.0.1";
+const port = 8000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Soy el grupo 4');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Soy el grupo 4");
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
