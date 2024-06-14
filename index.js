64fcb250bab39c1ab707be48680e5fc4114cdd5a
Gustavo <gustavo645.gg.gg@gmail.com>
2024-06-13 22:54:55 -0500

    prueba

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 8000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
+
   res.end("Soy el grupo 4");
 });
 server.listen(port, hostname, () => {
