41c6f7a9d617d60cb3bcea3e3a07e2c17e802fe4
Gustavo <gustavo645.gg.gg@gmail.com>
2024-06-13 23:28:36 -0500

    pruebas locales, añadi archivos .py de la clase

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
-  res.end("Soy el grupo 4");
+  // let arreglo = [
+  //   {
+  //     nombre: "Gustavo",
+  //     apellido: "Plasencia",
+  //   },
+  //   {
+  //     nombre: "Pepe",
+  //     apellido: "Lucho",
+  //   },
+  // ];
+  let arreglo = [
+    [0, 1],
+    [0, 0],
+    [1, 0],
+    [1, 1],
+  ];
+
+  res.end(JSON.stringify(arreglo));
+
+  // res.end("Soy el grupo 4");
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
